9f1245a282854b89a656327ff98714bbd4f19700
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-04-23 22:50:41 +0200

    test events

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,67 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from '@workadventure/scripting-api-extra';
+import { ActionMessage } from "@workadventure/iframe-api-typings";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
+let currentActionMessage: ActionMessage | undefined = undefined;
 
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ', WA.player.tags);
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
-    WA.room.onEnterLayer('start').subscribe(() => {
-      WA.ui.modal.openModal(
-        {
-          title: 'WorkAdventure website',
-          src: window.location.origin,
-          allow: 'fullscreen',
-          allowApi: true,
-          position: 'center',
-        },
-        () => {
-          console.log('Modal closed');
-        }
-      );
+    WA.room.onEnterLayer("start").subscribe(() => {
+      if (!WA.player.state.loadVariable("ftue")) {
+        WA.ui.modal.openModal(
+          {
+            title: "WorkAdventure website",
+            src: `${window.location.origin}/modals/modal.html`,
+            allow: null,
+            allowApi: true,
+            position: "center",
+          },
+          () => {
+            WA.player.state.saveVariable("ftue", true, {
+              public: false,
+              scope: "world",
+              persist: true,
+            });
+          }
+        );
+      }
 
-      WA.chat.sendChatMessage(
-        `${WA.player.name} a rejoint le monde de FlopStory !`
-      );
+      // WA.chat.sendChatMessage(
+      //   `${WA.player.name} a rejoint le monde de FlopStory !`
+      // );
 
-      WA.ui.displayActionMessage({
-        message: 'Bienvenue dans le monde de FlopStory !',
-        callback: () => {},
-      });
+      // if (!currentActionMessage) {
+      //   currentActionMessage = WA.ui.displayActionMessage({
+      //     message: "Bienvenue dans le monde de FlopStory !",
+      //     callback: () => {},
+      //   });
+
+      //   setTimeout(() => {
+      //     currentActionMessage?.remove();
+      //   }, 1500);
+      // }
     });
 
-    WA.room.area.onEnter('landing').subscribe(() => {
-      WA.ui.displayActionMessage({
-        message: 'Bienvenue dans le monde de FlopStory !',
-        callback: () => {},
-      });
-    });
-
-    WA.room.area.onEnter('clock').subscribe(() => {
+    WA.room.area.onEnter("clock").subscribe(() => {
       const today = new Date();
-      const time = today.getHours() + ':' + today.getMinutes();
-      currentPopup = WA.ui.openPopup('clockPopup', "It's " + time, []);
+      const time = today.getHours() + ":" + today.getMinutes();
+      currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup);
+    WA.room.area.onLeave("clock").subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra()
       .then(() => {
-        console.log('Scripting API Extra ready');
+        console.log("Scripting API Extra ready");
       })
       .catch((e) => console.error(e));
   })
