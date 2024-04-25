a776b0caf4f6d7348b6b646e01714250d0694395
Myraedia <167860925+Myraedia@users.noreply.github.com>
2024-04-25 10:49:21 +0200

    test map

---
+++ tilesets/Exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Exterior" tilewidth="192" tileheight="192" tilecount="1508" columns="29">
+ <image source="Exterior.png" width="5632" height="10144"/>
+</tileset>
---
+++ tilesets/Int_Office.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Int_Office" tilewidth="192" tileheight="192" tilecount="16" columns="2">
+ <image source="Int_Office.png" width="512" height="1696"/>
+</tileset>
