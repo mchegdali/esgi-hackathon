31a55a204459bfaa9196d4fdeecf758bc84dbfa9
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-04-26 11:32:30 +0200

    rework state mgmt

---
+++ src/events/manage-users.ts
@@ -0,0 +1,56 @@
+import { PLAYERS_COUNT_NEEDED, eventAreas } from '../config';
+
+export default function manageUsers() {
+  for (const { areaName, variableName, btnId, eventName } of eventAreas) {
+    if (WA.player.tags.includes('admin')) {
+      const playerCountVariableName = `playerCount:${areaName}`;
+      WA.players.onVariableChange(variableName).subscribe(({ value }) => {
+        const currentPlayerCount = WA.player.state.loadVariable(
+          playerCountVariableName
+        ) as number;
+
+        const newPlayerCount = !!value
+          ? currentPlayerCount + 1
+          : currentPlayerCount - 1;
+
+        WA.player.state.saveVariable(playerCountVariableName, newPlayerCount, {
+          public: false,
+          scope: 'room',
+          persist: false,
+        });
+      });
+
+      WA.player.state
+        .onVariableChange(playerCountVariableName)
+        .subscribe((value) => {
+          WA.ui.actionBar.removeButton(btnId);
+          if (value === PLAYERS_COUNT_NEEDED) {
+            WA.ui.actionBar.addButton({
+              id: btnId,
+              label: `Lancer le jeu <strong>${eventName}</strong>`,
+
+              callback: () => {
+                console.log(`start event: ${eventName}`);
+              },
+            });
+          }
+        });
+    } else {
+      WA.room.area.onEnter(areaName).subscribe(() => {
+        WA.player.state.saveVariable(variableName, true, {
+          public: true,
+          scope: 'room',
+          persist: false,
+        });
+      });
+
+      WA.room.area.onLeave(areaName).subscribe(() => {
+        WA.player.state.saveVariable(variableName, false, {
+          public: true,
+          scope: 'room',
+          persist: false,
+        });
+      });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from '@workadventure/scripting-api-extra';
-import initFlopStoriesEvent from './events/flop-stories/init';
 import onFirstTimeEnter from './events/on-first-time-enter';
+import manageUsers from './events/manage-users';
 
 console.log('Script started successfully');
 
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
-    document.styleSheets.item(0)?.insertRule('#');
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags);
 
     onFirstTimeEnter();
-    initFlopStoriesEvent();
-
+    manageUsers();
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra()
       .then(() => {
