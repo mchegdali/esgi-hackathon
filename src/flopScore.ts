a564dd797a7bb609b313a7367ed9d9ac890c743b
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-04-27 00:02:35 +0200

    fix(types)

--- src/flopScore.ts
+++ src/flopScore.ts
@@ -1,69 +1,42 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import { closeModal } from "./main";
+WA.onInit().then(async () => {
+  console.log("Script started successfully flopStory");
+  await WA.players.configureTracking();
 
+  //   function submitFlopStory() {
+  //     const vote = new Map();
+  //     const tabScore = [];
 
+  //     const form = document.getElementById("flopForm");
+  //     if (form) {
+  //       form.addEventListener("submit", async (e) => {
+  //         e.preventDefault();
+  //         const fd = new FormData(e.target as HTMLFormElement);
+  //         const flop = fd.get("flop");
+  //         tabScore.push(flop);
 
-console.log('Script started successfully flopStory');
-await WA.players.configureTracking();
-const players = WA.players.list();
-for (const player of players) {
-    console.log(`Player ${player.name} score is ${player.state.vote}`);
-}
- function submitFlopStory() {
-    let vote= new Map();
-    const tabScore = [];
-    let joueur = WA.player.uuid;
-    console.log(joueur);
-    const form = document.getElementById('flopForm');
-    if(form) {
-        form.addEventListener("submit",async (e)=> {
-            e.preventDefault();
-            const fd = new FormData(e.target as HTMLFormElement);
-            const flop = fd.get('flop');
-            tabScore.push(flop)
-            console.log("res",flop)
-            vote.set(joueur,vote );
-            await WA.player.state.saveVariable("flop-stories-questions",vote)
-            console.log("variable workadventure",WA.player.state.loadVariable("flop-stories-questions"));
-            console.log("all player :",WA.players.list());
-            const players = WA.players.list();
-            
-            WA.ui.modal.closeModal();
-            
-           
-        })
-    }
-   else {
-    console.log("veuillez envoyer votre formulaire");
-    return 0;
-   }
-}
+  //         vote.set(WA.player.uuid, vote);
+  //         await WA.player.state.saveVariable("flop-stories-questions", vote);
 
-function gestionScore() {
-    let score = new Map();
-    let joueur = WA.player.uuid;
-    // Obtenir le choix de l'utilisateur depuis la fonction submitFlopStory
-    const selectedFlop = submitFlopStory();
-    console.log(selectedFlop);
-    // Vérifier si un choix a été fait
-    if (selectedFlop) {
-        // Mettre à jour le score du joueur
-        score.set("joueur", selectedFlop);
-        console.log("Le vote est :", score.get("joueur") , "de" , joueur );
-        
-    } else {
-        console.log("Aucun choix n'a été fait.");
-    }
-}
+  //         WA.ui.modal.closeModal();
+  //       });
+  //     }
+  //   }
 
-// Appeler la fonction pour dynamiser le contenu du h1 lors du chargement de la page
-window.onload = function() {
-    WA.onInit().then(() => {
-        submitFlopStory();
-        gestionScore();
-    });
-    
-};
+  //   function gestionScore() {
+  //     const score = new Map();
+  //     let joueur = WA.player.uuid;
+  //     // Obtenir le choix de l'utilisateur depuis la fonction submitFlopStory
+  //     const selectedFlop = submitFlopStory();
 
+  //     // Vérifier si un choix a été fait
+  //     if (selectedFlop) {
+  //       // Mettre à jour le score du joueur
+  //       score.set("joueur", selectedFlop);
+  //       console.log("Le vote est :", score.get("joueur"), "de", joueur);
+  //     } else {
+  //       console.log("Aucun choix n'a été fait.");
+  //     }
+  //   }
+});
